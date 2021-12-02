bfa81a12ff8ded5c8aa3f73e5bb609a80f3a5779
Gustavo Henrique <82879034+Gustavo010@users.noreply.github.com>
2021-12-02 11:23:10 -0300

    Update script.sql

--- trabalhoSQL/script.sql
+++ trabalhoSQL/script.sql
@@ -9,3 +9,15 @@ create table funcionarios(
     Data_Desligamento date not null,
     Ultimo_Salario decimal(9,2) not null
 );
+
+LOAD DATA INFILE 'C:\Users\Aluno.DSN5131112167\Desktop\trabalhoSQL\aula.csv'  
+
+INTO TABLE funcionarios
+
+FIELDS TERMINATED BY ';'
+
+ENCLOSED BY '"'
+
+LINES TERMINATED BY '\r\n'
+
+IGNORE 1 ROWS;
